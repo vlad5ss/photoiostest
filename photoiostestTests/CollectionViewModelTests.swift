51e7dadf4b01897596876d390c78eb32107a1f33
vlad5ss <vlad5grup@gmail.com>
2017-09-21 12:25:54 +0400

    Added testPerformInitialViewSetup_Call

--- photoiostestTests/CollectionViewModelTests.swift
+++ photoiostestTests/CollectionViewModelTests.swift
@@ -59,4 +59,19 @@ extension CollectionViewModelTests {
         
         self.waitForExpectations(timeout: 1.0, handler: nil)
         
-    }}
+    }
+    
+    func testPerformInitialViewSetup_Calls_SetSectionInset_OnCollectionViewController() {
+        let expectation = self.expectation(description: "expected setSectionInset() to be called")
+        mockCollectionViewController!.expectationForSetSectionInset = expectation
+        
+        let viewModel = CollectionViewModel(view:mockCollectionViewController!)
+        viewModel.performInitialViewSetup()
+        
+        self.waitForExpectations(timeout: 1.0, handler: nil)
+        
+    }
+
+
+
+}
